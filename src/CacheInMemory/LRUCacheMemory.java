93c17763c973df39f4c15c88fe4c5a8386c99e50
ur licoris <kuzminykh6@gmail.com>
2022-07-02 10:48:57 +0400

    update put and get methods, improving code readability

--- src/CacheInMemory/LRUCacheMemory.java
+++ src/CacheInMemory/LRUCacheMemory.java
@@ -1,6 +1,7 @@
 package CacheInMemory;
 
 import CacheInterface.Cache;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,12 +9,12 @@ import java.util.Optional;
 
 public class LRUCacheMemory<K extends String, V extends String> implements Cache<K, V>
     {
+        private Node<K, V> mostRecentlyUsed;
+        private Node<K, V> leastRecentlyUsed;
         private int currentSize;
         private int maxSize;
-        private Node<K, V> mostRecentlyUsed;
-
-        private Node<K, V> leastRecentlyUsed;
         private Map<K, Node<K, V>> cacheElementsMap;
+        private Node<K, V> dummyNode;
 
 
         public LRUCacheMemory(int maxSize)
@@ -26,75 +27,98 @@ public class LRUCacheMemory<K extends String, V extends String> implements Cache
             }
 
         @Override
-        public void put(K key, V value)
+        public boolean put(K key, V value)
             {
-                //Если ключ уже содержится, то выходим из метода
-                if (cacheElementsMap.containsKey(key)) {
-                    System.out.println("Key already exists.");
-                    return;
+                Node<K, V> newNode;
+
+                if (this.cacheElementsMap.containsKey(key)) {
+                    Node<K, V> existingNode = cacheElementsMap.get(key);
+                    this.updateAndMoveToFront(existingNode, value);
+                    return true;
+                } else {
+                    newNode = new Node<>(mostRecentlyUsed, null, key, value);
+                    makeNewMRU(newNode);
                 }
 
-                //Создаем новый узел по заданному ключу и значению
-                Node<K, V> newNode = new Node<>(mostRecentlyUsed, null, key, value);
+                if (newNode.isEmpty()) return false;
 
-                //меняем ссылку старого mru на уже новый mru
-                mostRecentlyUsed.next = newNode;
-                //Добавляем узел в мапу
-                cacheElementsMap.put(key, newNode);
-                //указываем значение нового mru
-                mostRecentlyUsed = newNode;
-
-                //проверяем достигнут ли максимальный размер,если да - удаляем lru
                 if (currentSize == maxSize) {
-                    cacheElementsMap.remove(leastRecentlyUsed.key);
-                    leastRecentlyUsed = leastRecentlyUsed.next;
-                    leastRecentlyUsed.previous = null;
-                }//поинтер lru кидаем на самый 1 элемент, до 1 get или до заполнения кэша
+                    this.evictElement();
+                }//поинтер lru кидаем на самый 1 элемент, до вызова get или до заполнения кэша
                 else if (currentSize < maxSize) {
                     if (currentSize == 0) {
                         leastRecentlyUsed = newNode;
                     }
-
                     currentSize++;
                 }
+                return true;
+            }
+
+        public void makeNewMRU(Node<K, V> newNode)
+            {
+                mostRecentlyUsed.setNext(newNode);
+                cacheElementsMap.put(newNode.getKey(), newNode);
+                mostRecentlyUsed = newNode;
+            }
+
+        public void evictElement()
+            {
+                cacheElementsMap.remove(leastRecentlyUsed.getKey());
+                leastRecentlyUsed = leastRecentlyUsed.getNext();
+                leastRecentlyUsed.getPrevious().setPrevious(null);
+            }
+
+        public void updateAndMoveToFront(@NotNull Node<K, V> node, V value)
+            {
+                node.setValue(value);
+
+                if (node.isEmpty())
+                    return;
+
+                Node<K, V> nextNode = node.getNext();
+                Node<K, V> previousNode = node.getPrevious();
+
+                //если узел является lru , то передаем поинтер lru следующему узлу
+                if (node == leastRecentlyUsed) {
+                    nextNode.getPrevious().setNext(null);
+                    leastRecentlyUsed = nextNode;
+                }//если узел не является ни lru ни mru , то просто связываем узлы на которые он ссылается.
+                else {
+                    nextNode.getPrevious().setPrevious(previousNode);
+                    previousNode.getNext().setNext(nextNode);
+                }
+                //делаем наше значение mru
+                mostRecentlyUsed.setNext(node);
+                node.setNext(null);
+                mostRecentlyUsed = node;
+
             }
 
 
         @Override
         public Optional<V> get(K key)
             {
-                //получаем значение ключа из хэшмап
+
                 Node<K, V> tempNode = cacheElementsMap.get(key);
 
-                //проверяем существует ли ключ
-                if (tempNode == null) {
+                if (tempNode == null || tempNode.isEmpty()) {
                     return Optional.empty();
                 }
-                //если значение mru , просто возвращаем его
                 else if (tempNode == mostRecentlyUsed) {
-                    return Optional.ofNullable(tempNode.value);
+                    return Optional.of(tempNode.getValue());
                 }
 
-                //создаем значения для связки 2 узлов после и перед нашим основным узлом.
-                Node<K, V> nextNode = tempNode.next;
-                Node<K, V> previousNode = tempNode.previous;
+                this.moveToFront(tempNode);
 
-                //если узел является lru , то передаем поинтер lru следующему узлу
-                if (tempNode == leastRecentlyUsed) {
-                    nextNode.previous = null;
-                    leastRecentlyUsed = nextNode;
-                }//если узел не является ни lru ни mru , то просто связываем узлы на которые он ссылается.
-                else {
-                    nextNode.previous = previousNode;
-                    previousNode.next = nextNode;
+
+                return Optional.of(tempNode.getValue());
+            }
+
+        public void moveToFront(@NotNull Node<K, V> node)
+            {
+                if (!node.isEmpty()) {
+                    updateAndMoveToFront(node, node.getValue());
                 }
-
-                //делаем наше значение mru
-                mostRecentlyUsed.next = tempNode;
-                tempNode.next = null;
-                mostRecentlyUsed = tempNode;
-
-                return Optional.ofNullable(tempNode.value);
             }
 
 
--- src/CacheInterface/Cache.java
+++ src/CacheInterface/Cache.java
@@ -1,23 +1,67 @@
 package CacheInterface;
 
-import java.util.Map;
+import java.io.Serializable;
 import java.util.Optional;
 
 public interface Cache<K, V>
     {
-        void put(K key, V value);
+        boolean put(K key, V value);
 
         Optional<V> get(K key);
 
         void clearCache();
 
-        class Node<K, V>
+        class Node<K, V> implements Serializable
             {
-                public Node<K, V> previous;
-                public Node<K, V> next;
-                public K key;
-                public V value;
+                private Node<K, V> previous;
+                private Node<K, V> next;
+                private K key;
+                private V value;
 
+                public K getKey()
+                    {
+                        return key;
+                    }
+
+                public void setKey(K key)
+                    {
+                        this.key = key;
+                    }
+
+                public V getValue()
+                    {
+                        return value;
+                    }
+
+                public void setValue(V value)
+                    {
+                        this.value = value;
+                    }
+
+                public Node<K, V> getPrevious()
+                    {
+                        return previous;
+                    }
+
+                public void setPrevious(Node<K, V> previous)
+                    {
+                        this.previous = previous;
+                    }
+
+                public Node<K, V> getNext()
+                    {
+                        return next;
+                    }
+
+                public void setNext(Node<K, V> next)
+                    {
+                        this.next = next;
+                    }
+
+                public boolean isEmpty()
+                    {
+                        return this.value == null;
+                    }
 
                 public Node(Node<K, V> previous, Node<K, V> next, K key, V value)
                     {
